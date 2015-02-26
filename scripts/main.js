225e4b18bd80324a6670a42cb8a9a79f29d0f60a
Eric Lehmann <elehmann@cw_185.thecementworks.com>
2015-02-26 17:35:08 -0500

    some work figuring out how to dynamically populate product list

---
+++ scripts/main.js
@@ -0,0 +1,26 @@
+// BIKES
+
+// var dynContent = document.getElementById('dynamicContent');
+// var compiledLisst = "";
+// var spec1 = "<div class="col-md-6 col-sm-12 testBorder">spec1</div>";
+// var spec2 = "<div class="col-md-6 col-sm-12 testBorder">spec2</div>";
+// dynContent.innerHTML = spec1;
+
+
+// var dynContent = document.getElementById('dynamicContent');
+// var compiledList = "";
+// var spec1 = "<div class="col-md-6 col-sm-12 testBorder">spec1</div>";
+// var spec2 = "<div class="col-md-6 col-sm-12 testBorder">spec2</div>";
+
+// var testDIV = document.createElement('div');
+// testDIV.innerHTML = <p> TEEEEEESSSSTTTT</p>;
+// $("#dynamicContent").appendChild(testDIV);
+
+
+
+$(function() {
+	
+	$("#dynamicContent").html("TEEEEEESSSSTTTT!!!!!");
+})
+
+
