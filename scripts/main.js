2f89e347b28442a13e468d893d6ed8e57ab15215
Eric Lehmann <elehmann@thebloc.com>
2015-02-27 03:06:36 -0500

    close to getting sorter working

--- scripts/main.js
+++ scripts/main.js
@@ -4,6 +4,178 @@ var brand;
 var type;
 var price;
 
+// THUMBNAIL HTML
+
+var feltMt1_thumbId = "feltMt1";
+var feltMt1_thumbImg = "img/bikes/felt_mountain_1_thumb.jpg";
+var feltMt1_name = "Felt Mountain 1";
+var feltMt1_price = 400;
+var feltMt1 = [feltMt1_thumbId, feltMt1_thumbImg, feltMt1_name, feltMt1_price];
+
+var feltMt2_thumbId = "feltMt2";
+var feltMt2_thumbImg = "img/bikes/felt_mountain_2_thumb.jpg";
+var feltMt2_name = "Felt Mountain 2";
+var feltMt2_price = 700;
+var feltMt2 = [feltMt2_thumbId, feltMt2_thumbImg, feltMt2_name, feltMt2_price];
+
+var feltMt3_thumbId = "feltMt3";
+var feltMt3_thumbImg = "img/bikes/felt_mountain_3_thumb.jpg";
+var feltMt3_name = "Felt Mountain 3";
+var feltMt3_price = 900;
+var feltMt3 = [feltMt3_thumbId, feltMt3_thumbImg, feltMt3_name, feltMt3_price];
+
+var feltMt4_thumbId = "feltMt4";
+var feltMt4_thumbImg = "img/bikes/felt_mountain_4_thumb.jpg";
+var feltMt4_name = "Felt Mountain 4";
+var feltMt4_price = 2000;
+var feltMt4 = [feltMt4_thumbId, feltMt4_thumbImg, feltMt4_name, feltMt4_price];
+
+var feltMt5_thumbId = "feltMt5";
+var feltMt5_thumbImg = "img/bikes/felt_mountain_5_thumb.jpg";
+var feltMt5_name = "Felt Mountain 5";
+var feltMt5_price = 4000;
+var feltMt5 = [feltMt5_thumbId, feltMt5_thumbImg, feltMt5_name, feltMt5_price];
+
+var feltRd1_thumbId = "feltRd1";
+var feltRd1_thumbImg = "img/bikes/felt_road_1_thumb.jpg";
+var feltRd1_name = "Felt Road 1";
+var feltRd1_price = 350;
+var feltRd1 = [feltRd1_thumbId, feltRd1_thumbImg, feltRd1_name, feltRd1_price];
+
+var feltRd2_thumbId = "feltRd2";
+var feltRd2_thumbImg = "img/bikes/felt_road_2_thumb.jpg";
+var feltRd2_name = "Felt Road 2";
+var feltRd2_price = 400;
+var feltRd2 = [feltRd2_thumbId, feltRd2_thumbImg, feltRd2_name, feltRd2_price];
+
+var feltRd3_thumbId = "feltRd3";
+var feltRd3_thumbImg = "img/bikes/felt_road_3_thumb.jpg";
+var feltRd3_name = "Felt Road 3";
+var feltRd3_price = 600;
+var feltRd3 = [feltRd3_thumbId, feltRd3_thumbImg, feltRd3_name, feltRd3_price];
+
+var feltRd4_thumbId = "feltRd4";
+var feltRd4_thumbImg = "img/bikes/felt_road_4_thumb.jpg";
+var feltRd4_name = "Felt Road 4";
+var feltRd4_price = 800;
+var feltRd4 = [feltRd4_thumbId, feltRd4_thumbImg, feltRd4_name, feltRd4_price];
+
+var feltRd5_thumbId = "feltRd5";
+var feltRd5_thumbImg = "img/bikes/felt_road_5_thumb.jpg";
+var feltRd5_name = "Felt Road 5";
+var feltRd5_price = 1900;
+var feltRd5 = [feltRd5_thumbId, feltRd5_thumbImg, feltRd5_name, feltRd5_price];
+
+var feltTr1_thumbId = "feltTr1";
+var feltTr1_thumbImg = "img/bikes/felt_track_1_thumb.jpg";
+var feltTr1_name = "Felt Track 1";
+var feltTr1_price = 900;
+var feltTr1 = [feltTr1_thumbId, feltTr1_thumbImg, feltTr1_name, feltTr1_price];
+
+var feltTr2_thumbId = "feltTr2";
+var feltTr2_thumbImg = "img/bikes/felt_track_2_thumb.jpg";
+var feltTr2_name = "Felt Track 2";
+var feltTr2_price = 3600;
+var feltTr2 = [feltTr2_thumbId, feltTr2_thumbImg, feltTr2_name, feltTr2_price];
+
+var feltTr3_thumbId = "feltTr3";
+var feltTr3_thumbImg = "img/bikes/felt_track_3_thumb.jpg";
+var feltTr3_name = "Felt Track 3";
+var feltTr3_price = 4800;
+var feltTr3 = [feltTr3_thumbId, feltTr3_thumbImg, feltTr3_name, feltTr3_price];
+
+var konaRd1_thumbId = "konaRd1";
+var konaRd1_thumbImg = "img/bikes/kona_road_1_thumb.jpg";
+var konaRd1_name = "Kona Road 1";
+var konaRd1_price = 2900
+var konaRd1 = [konaRd1_thumbId, konaRd1_thumbImg, konaRd1_name, konaRd1_price];
+
+var scottMt1_thumbId = "scottMt1";
+var scottMt1_thumbImg = "img/bikes/scott_mountain_1_thumb.png";
+var scottMt1_name = "Scott Mountain 1";
+var scottMt1_price = 450;
+var scottMt1 = [scottMt1_thumbId, scottMt1_thumbImg, scottMt1_name, scottMt1_price];
+
+var scottMt2_thumbId = "scottMt2";
+var scottMt2_thumbImg = "img/bikes/scott_mountain_2_thumb.png";
+var scottMt2_name = "Scott Mountain 2";
+var scottMt2_price = 800;
+var scottMt2 = [scottMt2_thumbId, scottMt2_thumbImg, scottMt2_name, scottMt2_price];
+
+var scottMt3_thumbId = "scottMt3";
+var scottMt3_thumbImg = "img/bikes/scott_mountain_3_thumb.png";
+var scottMt3_name = "Scott Mountain 3";
+var scottMt3_price = 900;
+var scottMt3 = [scottMt3_thumbId, scottMt3_thumbImg, scottMt3_name, scottMt3_price];
+
+var scottMt4_thumbId = "scottMt4";
+var scottMt4_thumbImg = "img/bikes/scott_mountain_4_thumb.png";
+var scottMt4_name = "Scott Mountain 4";
+var scottMt4_price = 1750;
+var scottMt4 = [scottMt4_thumbId, scottMt4_thumbImg, scottMt4_name, scottMt4_price];
+
+var scottMt5_thumbId = "scottMt5";
+var scottMt5_thumbImg = "img/bikes/scott_mountain_5_thumb.png";
+var scottMt5_name = "Scott Mountain 5";
+var scottMt5_price = 3100;
+var scottMt5 = [scottMt5_thumbId, scottMt5_thumbImg, scottMt5_name, scottMt5_price];
+
+var scottRd1_thumbId = "scottRd1";
+var scottRd1_thumbImg = "img/bikes/scott_road_1_thumb.png";
+var scottRd1_name = "Scott Road 1";
+var scottRd1_price = 300;
+var scottRd1 = [scottRd1_thumbId, scottRd1_thumbImg, scottRd1_name, scottRd1_price];
+
+var scottRd2_thumbId = "scottRd2";
+var scottRd2_thumbImg = "img/bikes/scott_road_2_thumb.png";
+var scottRd2_name = "Scott Road 2";
+var scottRd2_price = 475;
+var scottRd2 = [scottRd2_thumbId, scottRd2_thumbImg, scottRd2_name, scottRd2_price];
+
+var scottRd3_thumbId = "scottRd3";
+var scottRd3_thumbImg = "img/bikes/scott_road_3_thumb.png";
+var scottRd3_name = "Scott Road 3";
+var scottRd3_price = 775;
+var scottRd3 = [scottRd3_thumbId, scottRd3_thumbImg, scottRd3_name, scottRd3_price];
+
+var scottRd4_thumbId = "scottRd4";
+var scottRd4_thumbImg = "img/bikes/scott_road_4_thumb.png";
+var scottRd4_name = "Scott Road 4";
+var scottRd4_price = 1800;
+var scottRd4 = [scottRd4_thumbId, scottRd4_thumbImg, scottRd4_name, scottRd4_price];
+
+var allBikesArray = [feltMt1, feltMt2, feltMt3, feltMt4, feltMt5, feltRd1, feltRd2, feltRd3, feltRd4, feltRd5, feltTr1, feltTr2, feltTr3, 
+	konaRd1, scottMt1, scottMt2, scottMt3, scottMt4, scottMt5, scottRd1, scottRd2, scottRd4];
+var feltArray = [feltMt1, feltMt2, feltMt3, feltMt4, feltMt5, feltRd1, feltRd2, feltRd3, feltRd4, feltRd5, feltTr1, feltTr2, feltTr3];
+var konaArray = [konaRd1];
+var scottArray = [scottMt1, scottMt2, scottMt3, scottMt4, scottMt5, scottRd1, scottRd2, scottRd4];
+var mountainArray = [feltMt1, feltMt2, feltMt3, feltMt4, feltMt5, scottMt1, scottMt2, scottMt3, scottMt4, scottMt5];
+var roadArray = [feltRd1, feltRd2, feltRd3, feltRd4, feltRd5, konaRd1, scottRd1, scottRd2, scottRd4];
+var trackArray = [feltTr1, feltTr2, feltTr3];
+var to500Array = [feltMt1, feltRd1, feltRd2, scottMt1, scottRd1, scottRd2];
+var to1000Array = [feltMt2, feltMt3, feltRd3, feltRd4, feltTr1, scottMt2, scottMt3, scottRd4];
+var to5000Array = [feltMt4, feltMt5, feltRd5, feltTr2, feltTr3, konaRd1, scottMt4, scottMt5];
+
+
+
+
+
+var thumb1 = "<div class='col-md-3 col-sm-6 thumbBox' id='";
+var thumb2 = "'><center><img src='";
+var thumb3 = "' class='thumbImage'><h4>";
+var thumb4 = "</h4>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean auctor a turpis vel malesuada. </center></div>";
+
+var testBike = "";
+testBike += thumb1 + feltMt1[0] + thumb2 + feltMt1[1] + thumb3 + feltMt1[2] + thumb4;
+
+var dynContent = "";
+
+
+
+
+
+
+
 
 var compiledList = "";
 var spec1 = "<div class='col-md-6 col-sm-12 testBorder'>spec1</div>";
@@ -15,14 +187,84 @@ var spec2 = "<div class='col-md-6 col-sm-12 testBorder'>spec2</div>";
 
 
 $(function() {
-	var dynContent = "";
-	dynContent += spec1;
-	dynContent += spec2;
+
+	// TESTING //
+	//var dynContent = "";
+	//dynContent += spec1;
+	//dynContent += spec2;
 	//var testDIV = document.createElement(dynContent);
+	//$("#dynamicContent").html(testBike);
 	
-	
-	//$("#dynamicContent").html(dynContent);
-	
+
+	// PRODUCT SELECTION
+
+	$("#bikeForm").submit(function(e) {
+        e.preventDefault();
+        //var entName = document.forms["myForm"]["userName"].value;
+        //var entPass = document.forms["myForm"]["password"].value;
+        brand = document.forms["bikeForm"]["brandButton"].value;
+        type = document.forms["bikeForm"]["brandButton"].value;
+        price = document.forms["bikeForm"]["brandButton"].value;
+        var brandArray;
+        var typeArray;
+        var priceArray;
+
+
+
+        // FIND A NEW WAY TO DO THIS. 
+
+        if (brand === "felt") {
+        	brandArray = feltArray;
+        } else if (brand === "kona") {
+        	brandArray = konaArray;
+        } else if (brand === "scott") {
+        	brandArray = scottArray;
+        } else {
+        	brandArray = allBikesArray;
+        };
+
+        if (type === "mountain") {
+        	typeArray = mountainArray;
+        } else if (type === "road") {
+        	typeArray = roadArray;
+        } else if (type === "track") {
+        	typeArray = trackArray;
+        } else {
+        	typeArray = allBikesArray;
+        };
+
+        if (price === "to500") {
+        	priceArray = to500Array;
+        } else if (price === "to1000") {
+        	priceArray = to1000Array;
+        } else if (price === "to5000") {
+        	priceArray = to5000Array;
+        } else {
+        	priceArray = allBikesArray;
+        };
+
+    	
+
+    	for (var i = 0; i < brandArray.length; i++) {
+    		var currentBike = brandArray[i];
+    		// var brandContained = $.inArray(currentBike, brandArray);
+    		var typeContained = $.inArray(currentBike, typeArray);
+    		var priceContained = $.inArray(currentBike, priceArray)
+
+    		if (typeContained >= 0 && priceContained >= 0) {
+    			var bikeDiv = "";
+				bikeDiv += thumb1 + currentBike[0] + thumb2 + currentBike[1] + thumb3 + currentBike[2] + thumb4;
+    		};
+    		dynContent += bikeDiv;
+    		//console.log("brandContained " + brandContained);
+    		console.log("typeContained " + typeContained);
+    		console.log("priceContained " + priceContained);
+    		console.log(dynContent);
+    	};
+
+    	$("#dynamicContent").html(dynContent);
+
+    });
 
 
 	// LIGHTBOX CODE
