a464f20728bef56fd6563bc25dd2da64c67e2077
Eric Lehmann <elehmann@thebloc.com>
2015-03-08 23:49:46 -0400

    worked nav on my pages so they would work properly on small screens.

---
+++ scripts/contact.js
@@ -0,0 +1,12 @@
+// CONTACT PAGE
+
+function messageSubmit(){
+    var name = document.getElementById('nameField').value;
+    var subject = document.getElementById('subjectField').value;
+    var content = document.getElementById('contentField').value;
+
+    console.log("Name: " + name);
+    console.log("Subject: " + subject);
+    console.log("Content: " + content);
+    $("#messageButton").html("Message submitted");
+}
